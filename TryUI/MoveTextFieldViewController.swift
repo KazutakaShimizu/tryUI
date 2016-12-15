e3ae0e263d46f61f91f063d0d6591415970d9bfb
清水一貴 <s.kazutaka55555@gmail.com>
2016-12-15 10:31:42 +0900

    キーボードの出現に合わせて、スクロールビューを動かす

---
+++ TryUI/MoveTextFieldViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MoveTextFieldViewController.swift
+//  TryUI
+//
+//  Created by 清水一貴 on 2016/12/14.
+//  Copyright © 2016年 清水一貴. All rights reserved.
+//
+
+import UIKit
+
+class MoveTextFieldViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let nofitiShow = Notification.Name.UIKeyboardWillShow
+        let nofitiHide = Notification.Name.UIKeyboardWillHide
+        
+        // Notification の追加
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillShow(notification:)),
+            name: nofitiShow,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillHide(notification:)),
+            name: nofitiHide,
+            object: nil
+        )
+    }
+    
+    func keyboardWillShow(notification: Notification) {
+        if let userInfo = notification.userInfo {
+            
+            
+//            keyboardframeにはキーボードの位置やサイズの情報が、animationDurationにはアニメーションのジカンが入っている
+            if let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue, let animationDuration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as AnyObject).doubleValue {
+                
+                restoreScrollViewSize()
+                
+                let convertedKeyboardFrame = scrollView.convert(keyboardFrame, from: nil)
+                print(convertedKeyboardFrame)
+                let offsetY: CGFloat = textField.frame.maxY - convertedKeyboardFrame.minY
+                if offsetY < 0 { return }
+                updateScrollViewSize(moveSize: offsetY, duration: animationDuration)
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: Notification) {
+        restoreScrollViewSize()
+    }
+    
+    func updateScrollViewSize(moveSize: CGFloat, duration: TimeInterval) {
+        UIView.beginAnimations("ResizeForKeyboard", context: nil)
+        UIView.setAnimationDuration(duration)
+        
+        let contentInsets = UIEdgeInsetsMake(0, 0, moveSize, 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        scrollView.contentOffset = CGPoint(x: 0, y: moveSize)
+        
+        UIView.commitAnimations()
+    }
+    
+    func restoreScrollViewSize() {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+}
